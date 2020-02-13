4bc9f7aa85ef47c2f4905785d98b200351677a9c
nemalasrinu <cnu472@gmail.com>
2020-02-13 08:11:20 +0530

    initial code

---
+++ numbers.py
@@ -0,0 +1,37 @@
+# Summation Operator
+sum1 = 2 + 2 # int + int -> int
+sum2 = 10.0 + 3.0 # float + float -> float
+sum3 = 5 + 2.0 # int + float -> float
+print sum1, sum2, sum3
+
+# Differentiator Operator
+diff1 = 10 - 7 # int - int -> int
+diff2 = 10.0 - 3.0 # float - float -> float
+diff3 = 5 - 2.0 # int - float -> float
+print diff1, diff2, diff3
+
+# Multiplication Operator
+mul1 = 24 * 4 # int * int -> int
+mul2 = 10.0 * 3.0 # float * float -> float
+mul3 = 5 * 2.0 # int * float -> float
+print mul1, mul2, mul3
+
+# Division Operator
+div1 = 10 / 5 # int / int -> int( perform floor division )
+div2 = 17 / 3.0 # int / float -> float ( perform classic division )
+div3 = 17.0 / 3 # float / int -> float ( perform classic division )
+print div1, div2, div3
+
+# // - Floor division Operator doing floor division no matter about operands
+div4 = 10 // 5 # int / int -> int( perform floor division )
+div5 = 17 // 3.0 # int / float -> float ( perform floor division )
+div6 = 17.0 // 3 # float / int -> float ( perform floor division )
+print div4, div5, div6
+
+#% - Remainder Operator
+rem1 = 17 % 3
+rem2 = 17.0 % 3.0
+rem3 = 17 % 3.0
+rem4 = 17.0 % 3
+print rem1, rem2, rem3, rem4
+
